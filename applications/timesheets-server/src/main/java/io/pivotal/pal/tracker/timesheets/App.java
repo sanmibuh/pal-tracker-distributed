864a874f3a6e5947a0bb99f0b3b5aff2a5720c3c
David Belmez Vicente <sanmibuh@gmail.com>
2018-06-27 13:50:23 +0200

    [master]: lot of changes

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -1,16 +1,17 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import java.util.TimeZone;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
 
-import java.util.TimeZone;
-
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 public class App {
 
